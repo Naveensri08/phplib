e2769a7748cf3b2e14f888c3f2ebf345aee219ef
= <=>
2022-12-22 22:06:14 +0530

    Currency Conversion(number to word) class added on 22/12/2022

--- validate/Validate.php
+++ validate/Validate.php
@@ -8,7 +8,14 @@ class Validate
         // empty  constructor for this class.
     }
 
+    # IP whitelist check.
+    # Ex:- $ip = '127.0.0.1' and $allow_ip_array = array("192.168.0.1","127.0.0.1")
+    function isWhitelist($ip, $allow_ip_array) {
+        return in_array($ip, $allow_ip_array);
+    }
+
     # check if it is JSON.
+    # Ex:- {"testing":"sample"} 
     function isJson($string)
     {
         return ((is_string($string) &&
@@ -18,18 +25,19 @@ class Validate
 
     # Both Are Array variables
     # $a is payload and $b is payload names. 
+    # Ex:- $a = array('status'=> 1, 'name' => 'Naveenkumar') , $b = ['status', 'name'];
     function checkAllField($a, $b)
     {
         for ($i = 0; $i < count($b); $i++) {
             if (array_key_exists($b[$i], $a)) {
                 // echo $a[$b[$i]];
                 if (is_null($a[$b[$i]]) || empty($a[$b[$i]])) {
-                    return json_encode(array("error" => $b[$i] . " Not Be NULL"));
-                    // return false;
+                    echo json_encode(array("error" => $b[$i] . " Not Be NULL"));
+                    return false;
                 }
             } else {
-                return json_encode(array("error" => $b[$i] . " Is Missing"));
-                // return false;
+                echo json_encode(array("error" => $b[$i] . " Is Missing"));
+                return false;
             }
         }
         return true;
